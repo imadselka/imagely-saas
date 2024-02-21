39c2c79eb30025825d599d6c806359b145e7f947
Imad Selka <imadselkawi133@gmail.com>
2024-02-21 23:51:50 +0100

    fix image model

--- lib/database/models/image.model.ts
+++ lib/database/models/image.model.ts
@@ -1,6 +1,6 @@
 import { Document, Schema, model, models } from "mongoose";
 
-export interface Image extends Document {
+export interface IImage extends Document {
   title: string;
   transformationType: string;
   publicId: string;
@@ -8,66 +8,36 @@ export interface Image extends Document {
   width?: number;
   height?: number;
   config?: object;
-  transformationUrl?: URL;
+  transformationUrl?: string;
   aspectRatio?: string;
   color?: string;
   prompt?: string;
-  author?: {
+  author: {
     _id: string;
-    userName: string;
+    firstName: string;
     lastName: string;
   };
-  createdAt: Date;
-  updatedAt: Date;
+  createdAt?: Date;
+  updatedAt?: Date;
 }
 
-const imageSchema = new Schema({
-  title: {
-    type: String,
-    required: true,
-  },
-  transformationType: {
-    type: String,
-    required: true,
-  },
-  publicId: {
-    type: String,
-    required: true,
-  },
-  secureURL: {
-    type: String,
-    required: true,
-  },
-  width: {
-    type: Number,
-  },
-  height: {
-    type: Number,
-  },
-  config: {
-    type: Object,
-  },
-  transformationUr: {
-    type: String,
-  },
-  aspectRatio: {
-    type: String,
-  },
-  color: {
-    type: String,
-  },
+const ImageSchema = new Schema({
+  title: { type: String, required: true },
+  transformationType: { type: String, required: true },
+  publicId: { type: String, required: true },
+  secureURL: { type: String, required: true },
+  width: { type: Number },
+  height: { type: Number },
+  config: { type: Object },
+  transformationUrl: { type: String },
+  aspectRatio: { type: String },
+  color: { type: String },
   prompt: { type: String },
-  autor: { type: Schema.Types.ObjectId, ref: "User" },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  updatedAt: {
-    type: Date,
-    default: Date.now,
-  },
+  author: { type: Schema.Types.ObjectId, ref: "User" },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
 });
 
-const Image = models?.Image || model("Image", imageSchema);
+const Image = models?.Image || model("Image", ImageSchema);
 
 export default Image;

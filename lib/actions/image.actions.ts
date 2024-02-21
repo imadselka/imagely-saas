7a31a9f986ef03700f4f0c104781cf3c125fdadc
Imad Selka <imadselkawi133@gmail.com>
2024-02-21 23:53:41 +0100

    feat: add getAllImages, get Images By User id

--- lib/actions/image.actions.ts
+++ lib/actions/image.actions.ts
@@ -7,53 +7,65 @@ import User from "../database/models/user.model";
 import { connectToDatabase } from "../database/mongoose";
 import { handleError } from "../utils";
 
+import { v2 as cloudinary } from "cloudinary";
+
 const populateUser = (query: any) =>
   query.populate({
     path: "author",
     model: User,
-    select: "_id firstName lastName",
+    select: "_id firstName lastName clerkId",
   });
 
-// ADD IMAGE TO DB
+// ADD IMAGE
 export async function addImage({ image, userId, path }: AddImageParams) {
   try {
     await connectToDatabase();
 
     const author = await User.findById(userId);
-    // revalidate the path to update the cache with the new image added
-    // means to show the new image in the UI
-    if (!author) throw new Error("User not found");
+
+    if (!author) {
+      throw new Error("User not found");
+    }
+
     const newImage = await Image.create({
       ...image,
       author: author._id,
     });
+
     revalidatePath(path);
+
     return JSON.parse(JSON.stringify(newImage));
   } catch (error) {
     handleError(error);
   }
 }
 
-// Update Image
+// UPDATE IMAGE
 export async function updateImage({ image, userId, path }: UpdateImageParams) {
   try {
     await connectToDatabase();
 
-    const isImage = await Image.findById(image._id);
-    if (!isImage || isImage.author.toHexStriing() !== userId)
-      throw new Error("Unauthorized or Image not found");
-    const newImage = await Image.findByIdAndUpdate(isImage._id, image, {
-      new: true,
-    });
+    const imageToUpdate = await Image.findById(image._id);
+
+    if (!imageToUpdate || imageToUpdate.author.toHexString() !== userId) {
+      throw new Error("Unauthorized or image not found");
+    }
+
+    const updatedImage = await Image.findByIdAndUpdate(
+      imageToUpdate._id,
+      image,
+      { new: true }
+    );
 
     revalidatePath(path);
-    return JSON.parse(JSON.stringify(newImage));
+
+    return JSON.parse(JSON.stringify(updatedImage));
   } catch (error) {
     handleError(error);
   }
 }
 
-// Delete Image
+// DELETE IMAGE
 export async function deleteImage(imageId: string) {
   try {
     await connectToDatabase();
@@ -66,16 +78,109 @@ export async function deleteImage(imageId: string) {
   }
 }
 
-// Get Image
-
+// GET IMAGE
 export async function getImageById(imageId: string) {
   try {
     await connectToDatabase();
-    const getImage = await populateUser(Image.findById(imageId));
 
-    if (!getImage) throw new Error("Image not found");
+    const image = await populateUser(Image.findById(imageId));
 
-    return JSON.parse(JSON.stringify(getImage));
+    if (!image) throw new Error("Image not found");
+
+    return JSON.parse(JSON.stringify(image));
+  } catch (error) {
+    handleError(error);
+  }
+}
+
+// GET IMAGES
+export async function getAllImages({
+  limit = 9,
+  page = 1,
+  searchQuery = "",
+}: {
+  limit?: number;
+  page: number;
+  searchQuery?: string;
+}) {
+  try {
+    await connectToDatabase();
+
+    cloudinary.config({
+      cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+      api_key: process.env.CLOUDINARY_API_KEY,
+      api_secret: process.env.CLOUDINARY_API_SECRET,
+      secure: true,
+    });
+
+    let expression = "folder=imaginify";
+
+    if (searchQuery) {
+      expression += ` AND ${searchQuery}`;
+    }
+
+    const { resources } = await cloudinary.search
+      .expression(expression)
+      .execute();
+
+    const resourceIds = resources.map((resource: any) => resource.public_id);
+
+    let query = {};
+
+    if (searchQuery) {
+      query = {
+        publicId: {
+          $in: resourceIds,
+        },
+      };
+    }
+
+    const skipAmount = (Number(page) - 1) * limit;
+
+    const images = await populateUser(Image.find(query))
+      .sort({ updatedAt: -1 })
+      .skip(skipAmount)
+      .limit(limit);
+
+    const totalImages = await Image.find(query).countDocuments();
+    const savedImages = await Image.find().countDocuments();
+
+    return {
+      data: JSON.parse(JSON.stringify(images)),
+      totalPage: Math.ceil(totalImages / limit),
+      savedImages,
+    };
+  } catch (error) {
+    handleError(error);
+  }
+}
+
+// GET IMAGES BY USER
+export async function getUserImages({
+  limit = 9,
+  page = 1,
+  userId,
+}: {
+  limit?: number;
+  page: number;
+  userId: string;
+}) {
+  try {
+    await connectToDatabase();
+
+    const skipAmount = (Number(page) - 1) * limit;
+
+    const images = await populateUser(Image.find({ author: userId }))
+      .sort({ updatedAt: -1 })
+      .skip(skipAmount)
+      .limit(limit);
+
+    const totalImages = await Image.find({ author: userId }).countDocuments();
+
+    return {
+      data: JSON.parse(JSON.stringify(images)),
+      totalPages: Math.ceil(totalImages / limit),
+    };
   } catch (error) {
     handleError(error);
   }
